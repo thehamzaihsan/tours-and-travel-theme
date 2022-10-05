5b99217cff1c77f13d36879533a2e649a03e70ca
hamzaihsan <hamzaihsanpakistani@gmail.com>
2022-10-05 10:41:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var dropdown = document.querySelector('.dropdown');
+var navbarDropdown = document.querySelector('#navbarDropdown');
+var dropdownmenu = document.querySelector('.dropdown-menu');
+
+dropdown.addEventListener('mouseover' , function(){
+    navbarDropdown.classList.add('show');
+    dropdownmenu.classList.add('show');
+});
+
+
+dropdown.addEventListener('mouseout' , function(){
+    navbarDropdown.classList.remove('show');
+    dropdownmenu.classList.remove('show');
+});
+
+
+  
+  $('.owl-one').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: false,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: false,
+    dots: true,
+    center:false,
+    stagePadding: 10,
+    responsiveBaseElement: 'body',
+    animateOut: 'fadeOut',
+    nav:true,
+    responsive: {
+        0: {
+            items: 1,
+            center:true,
+            margin:30,
+            
+        },
+        770: {
+            items: 2,
+            
+        },
+        1000: {
+            items: 3,
+
+        }
+    }
+})
+
+
+
+    
