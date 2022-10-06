a918d5fdf43a14309707c89c5e5ea20d9c4aabfb
hamzaihsan <hamzaihsanpakistani@gmail.com>
2022-10-06 21:08:25 +0500

    more features added and sections updated

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var dropdown = document.querySelector('.dropdown');
 var navbarDropdown = document.querySelector('#navbarDropdown');
 var dropdownmenu = document.querySelector('.dropdown-menu');
+var owlnext = document.querySelector('.owl-next');
+var owlprev = document.querySelector('.owl-prev');
+
+
 
 dropdown.addEventListener('mouseover' , function(){
     navbarDropdown.classList.add('show');
@@ -13,38 +17,37 @@ dropdown.addEventListener('mouseout' , function(){
     dropdownmenu.classList.remove('show');
 });
 
-
-  
-  $('.owl-one').owlCarousel({
-    loop: true,
-    margin: 10,
-    autoplay: false,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: false,
-    dots: true,
-    center:false,
-    stagePadding: 10,
-    responsiveBaseElement: 'body',
-    animateOut: 'fadeOut',
-    nav:true,
-    responsive: {
-        0: {
-            items: 1,
-            center:true,
-            margin:30,
-            
-        },
-        770: {
-            items: 2,
-            
-        },
-        1000: {
-            items: 3,
-
-        }
-    }
-})
-
-
-
+$(document).ready(function(){
+    $('.owl-one').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: false,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: false,
+        dots: true,
+        center:false,
+        stagePadding: 10,
+        responsiveBaseElement: 'body',
+        animateOut: 'fadeOut',
+        nav:true,
+        responsive: {
+            0: {
+                items: 1,
+                center:true,
+                margin:30,
+                
+            },
+            770: {
+                items: 2,
+                
+            },
+            1000: {
+                items: 3,
     
+            }
+        }
+    })
+        
+  });
+  
+  
