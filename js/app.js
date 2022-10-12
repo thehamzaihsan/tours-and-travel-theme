6585796f995adf59013f7af0c36ace6de2c5222e
hamzaihsan <hamzaihsanpakistani@gmail.com>
2022-10-12 14:44:07 +0500

    fixed carousel

--- js/app.js
+++ js/app.js
@@ -47,7 +47,45 @@ $(document).ready(function(){
             }
         }
     })
-        
+
+    $('.owl-two').owlCarousel({
+        loop: true,
+        margin: 40,
+        autoplay: false,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: false,
+        dots: true,
+        center:false,
+        stagePadding: 10,
+        responsiveBaseElement: 'body',
+        animateOut: 'fadeOut',
+        nav:true,
+        responsive: {
+            0: {
+                items: 1,
+                center:true,
+                margin:30,
+                nav:true,
+                
+            },
+            770: {
+                items: 2,
+                nav:true,
+                
+            },
+            1000: {
+                items: 3,
+                nav:true,
+    
+            }
+        }
+    })
+
+    $('.owl-two').find('.owl-nav').removeClass('disabled');
+    $('.owl-two').on('changed.owl.carousel', function(event) {
+        $(this).find('.owl-nav').removeClass('disabled');
+    });
+   
   });
 
   
